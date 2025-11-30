bb98d50ac3c5f9a2d081735d8267183adbba30ce
daffz <dazht156@gmail.com>
2025-11-30 16:16:00 +0700

    Update smart_light_ldr.ino

--- smart_light_ldr.ino
+++ smart_light_ldr.ino
@@ -1,45 +1,22 @@
-#define BLYNK_PRINT Serial
-#define BLYNK_TEMPLATE_ID "YOUR_TEMPLATE_ID"
-#define BLYNK_DEVICE_NAME "Smart Light"
-#define BLYNK_AUTH_TOKEN "YOUR_BLYNK_AUTH_TOKEN"
+#define ldrPin 2
+#define led 7
 
-#include <WiFi.h>
-#include <BlynkSimpleEsp32.h>
-
-char auth[] = BLYNK_AUTH_TOKEN;
-char ssid[] = "YOUR_WIFI_SSID";
-char pass[] = "YOUR_WIFI_PASSWORD";
-
-#define LDRPIN 34
-#define LEDPIN 2
-
-bool manualOverride = false;
-int ledState = LOW;
-
-BLYNK_WRITE(V1)
-{
-  manualOverride = true;
-  ledState = param.asInt();
-  digitalWrite(LEDPIN, ledState);
+void setup() {
+  pinMode(ldrPin, INPUT);
+  pinMode(led, OUTPUT);
+  Serial.begin(9600);
 }
 
-void setup()
-{
-  Serial.begin(115200);
-  pinMode(LEDPIN, OUTPUT);
-  pinMode(LDRPIN, INPUT);
-  Blynk.begin(auth, ssid, pass);
-}
+void loop() {
+  int readFromLdr = digitalRead(ldrPin);
+  Serial.println(readFromLdr);
+  delay(200);
 
-void loop()
-{
-  if (manualOverride == false) {
-    int ldrValue = analogRead(LDRPIN);
-    if (ldrValue < 1500) {
-      digitalWrite(LEDPIN, HIGH);
-    } else {
-      digitalWrite(LEDPIN, LOW);
-    }
+  if (readFromLdr == LOW) {
+    digitalWrite(led, HIGH);
+    Serial.println("Kondisi Gelap => LED ON");
+  } else {
+    digitalWrite(led, LOW);
+    Serial.println("Kondisi Terang => LED OFF");
   }
-  Blynk.run();
 }
